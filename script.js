dfa9af7de8e2e0a88cd6b3d2ffed696df0c0018b
Girverius <gsmac1224@gmail.com>
2020-11-07 23:38:22 -0600

    corrections

--- script.js
+++ script.js
@@ -5,15 +5,13 @@ $(document).ready(function () {
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       cityName +
-      "&units=imperial&appid=d9776c23489e86b8af3aee508f8e7c15";
-    // var apiKey = "d9776c23489e86b8af3aee508f8e7c15";
+      "&units=imperial&appid=d9776c23489e86b8af3aee508f8e7c15"
 
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      // console.log(queryURL);
-      // console.log(response);
+  
       $("#current").empty();
       var currentDate = moment().format("L");
 
@@ -26,57 +24,9 @@ $(document).ready(function () {
       var windSpeedEl = $("<p>").text("Wind Speed: " + response.wind.speed);
       var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
       console.log(img);
-     
-      // console.log(currentWeather);
-      // if (currentWeather === "Clear Sky") {
-      //   var currentIcon = $("<img>").attr(
-      //     "src",
-      //     " https://openweathermap.org/img/wn/01d@2x.png"
-      //   );
-      //   currentIcon.attr("style", "height: 50px; width: 60px");
-      // } else if (currentWeather === "Rain") {
-      //   var currentIcon = $("<img>").attr(
-      //     "src",
-      //     " https://openweathermap.org/img/wn/10d@2x.png"
-      //   );
-      //   currentIcon.attr("style", "height: 50px; width: 50px");
-      // } else if (currentWeather === "Shower Rain") {
-      //   var currentIcon = $("<img>").attr(
-      //     "src",
-      //     " https://openweathermap.org/img/wn/09d@2x.png"
-      //   );
-      //   currentIcon.attr("style", "height: 50px; width: 50px");
-      // } else if (currentWeather === "thunderstorm") {
-      //   var currentIcon = $("<img>").attr(
-      //     "src",
-      //     " https://openweathermap.org/img/wn/11d@2x.png"
-      //   );
-      //   currentIcon.attr("style", "height: 50px; width: 50px");
-      // } else if (currentWeather === "snow") {
-      //   var currentIcon = $("<img>").attr(
-      //     "src",
-      //     " https://openweathermap.org/img/wn/13d@2x.png"
-      //   );
-      //   currentIcon.attr("style", "height: 50px; width: 50px");
-      // } else if (currentWeather === "few clouds") {
-      //   var currentIcon = $("<img>").attr(
-      //     "src",
-      //     " https://openweathermap.org/img/wn/02d@2x.png"
-      //   );
-      //   currentIcon.attr("style", "height: 50px; width: 50px");
-      // } else if (currentWeather === "mist") {
-      //   var currentIcon = $("<img>").attr(
-      //     "src",
-      //     " https://openweathermap.org/img/wn/50d@2x.png"
-      //   );
-      //   currentIcon.attr("style", "height: 50px; width: 60px");
-      // } else if (currentWeather === "overcast") {
-      //   var currentIcon = $("<img>").attr(
-      //     "src",
-      //     " https://openweathermap.org/img/wn/04d@2x.png"
-      //   );
-      //   currentIcon.attr("style", "height: 50px; width: 50px");
-      // }
+
+      
+
       //New div for current weather
       var newDiv = $("<div>");
       var uvlEl;
@@ -95,7 +45,7 @@ $(document).ready(function () {
         $("#uvl").empty();
         var uvlResults = response.value;
         uvlEl = $("<p>").text("UV Index:" + uvlResults);
-        // $("#uvl").html(uvlEl);
+
         newDiv.append(uvlEl);
 
     
@@ -108,89 +58,34 @@ $(document).ready(function () {
       url: queryURLforcast,
       method: "GET",
     }).then(function (response) {
-      // console.log(response)
+    
       $("#fiveDay").empty();
-      //var results = response.list;
+     
       for (var i = 0; i < response.list.length; i+= 8) {
         console.log(response.list[i]);
-        // if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+  
           var fiveForcast = $("<div>").addClass("card");
           $("#fiveDay").append(fiveForcast);
           var dayContent = $("<div>").addClass("card-body");
           fiveForcast.append(dayContent)
 
-          var date = response.list[i].dt_txt;
+        var date = response.list[i].dt_txt;
         var setDate = date.substr(0, 10);
         var temp = response.list[i].main.temp;
         var hum = response.list[i].main.humidity;
-        // var fiveForcast = $("<div>");
         var fiveH = $("<h5>").text(setDate).addClass("card-title setD");
         var fiveP = $("<p>").text(("Temp: " + temp)).addClass("card-text temp");
         var fiveP2 = $("<p>").text("Humidity " + hum).addClass("card-text humidity");
         
         
-        
         fiveForcast.addClass("card shadow-lg text-white bg-primary mx-auto mb-10 p-2");
-        //add text to tags
-        // $(".temp").text("Temp: " + temp);
-        // $(".humidity").text("Humidity " + hum);
-        // $(".setD").text(setDate);
-        
+      
         dayContent.append(fiveH);
         dayContent.append(fiveP);
         dayContent.append(fiveP2);
-        // var weather = response.list[i].weather[0].main;
-        // if (weather === "Clear Sky") {
-        //   var icon = $("<img>").attr(
-        //     "src",
-        //     " https://openweathermap.org/img/wn/01d@2x.png"
-        //   );
-        //   icon.attr("style", "height: 50px; width: 60px");
-        // } else if (weather === "Rain") {
-        //   var icon = $("<img>").attr(
-        //     "src",
-        //     " https://openweathermap.org/img/wn/10d@2x.png"
-        //   );
-        //   icon.attr("style", "height: 50px; width: 50px");
-        // } else if (weather === "Shower Rain") {
-        //   var icon = $("<img>").attr(
-        //     "src",
-        //     " https://openweathermap.org/img/wn/09d@2x.png"
-        //   );
-        //   icon.attr("style", "height: 50px; width: 50px");
-        // } else if (weather === "thunderstorm") {
-        //   var icon = $("<img>").attr(
-        //     "src",
-        //     " https://openweathermap.org/img/wn/11d@2x.png"
-        //   );
-        //   icon.attr("style", "height: 50px; width: 50px");
-        // } else if (weather === "snow") {
-        //   var icon = $("<img>").attr(
-        //     "src",
-        //     " https://openweathermap.org/img/wn/13d@2x.png"
-        //   );
-        //   icon.attr("style", "height: 50px; width: 50px");
-        // } else if (weather === "few clouds") {
-        //   var icon = $("<img>").attr(
-        //     "src",
-        //     " https://openweathermap.org/img/wn/02d@2x.png"
-        //   );
-        //   icon.attr("style", "height: 50px; width: 50px");
-        // } else if (weather === "mist") {
-        //   var icon = $("<img>").attr(
-        //     "src",
-        //     " https://openweathermap.org/img/wn/50d@2x.png"
-        //   );
-        //   icon.attr("style", "height: 50px; width: 60px");
-        // } else if (weather === "overcast") {
-        //   var icon = $("<img>").attr(
-        //     "src",
-        //     " https://openweathermap.org/img/wn/04d@2x.png"
-        //   );
-        //   icon.attr("style", "height: 50px; width: 50px");
-        // }
+       
         $("#fiveDay").append(fiveForcast);
-        // }
+
       }
     });
   }
@@ -213,8 +108,8 @@ $(document).ready(function () {
     var psearch = $("<button>")
    
     var ul = $("<ul>").attr("style","list-style: none;");
-    
-    $("#recentSearch").prepend(ul.append(li.append(psearch.append(recentSearch))));
+    var clearBtn = $("<button>")
+    $("#recentSearch").prepend(ul.append((li.append(psearch.append(recentSearch)))));
   }
   $("#recentSearch").on("click", "button", function (event) {
     event.preventDefault();
